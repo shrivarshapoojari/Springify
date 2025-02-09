08e71d00186cad5abfdab98268dd09542205e059
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-02-09 22:33:00 +0530

    setup

---
+++ Springify/src/main/java/com/shri/springify/Springify/SpringifyApplication.java
@@ -0,0 +1,13 @@
+package com.shri.springify.Springify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringifyApplication.class, args);
+	}
+
+}
