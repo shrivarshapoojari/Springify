3c853ada22ed2e36b998cca4b72c36e7b20a1901
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-02-11 22:32:59 +0530

    get profile endpoint complete

---
+++ Springify/src/main/java/com/shri/springify/Springify/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.shri.springify.Springify.controller;
+
+
+import com.shri.springify.Springify.model.User;
+import com.shri.springify.Springify.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+    @GetMapping("/profile")
+    public ResponseEntity<User>getUserDetails(
+            @RequestHeader("Authorization") String jwt
+    )
+    {
+        try{
+            User user=userService.findUserByJwt(jwt);
+            System.out.println(jwt);
+            return  new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
