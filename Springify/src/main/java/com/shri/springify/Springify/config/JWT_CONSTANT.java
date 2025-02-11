3cef4abcdb46c039984d8636bdad7a5a2f218a7c
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-02-11 05:58:55 +0530

    progressing with  jwt

---
+++ Springify/src/main/java/com/shri/springify/Springify/config/JWT_CONSTANT.java
@@ -0,0 +1,7 @@
+package com.shri.springify.Springify.config;
+
+public class JWT_CONSTANT {
+    public static  final  String secret="abdcfmdifejmslkcmscslcjmksevcjdnmvcnec";
+    public static  final String JWT_HEADER="Authorization";
+
+}
---
+++ Springify/src/main/java/com/shri/springify/Springify/config/JwtTokenValidator.java
@@ -0,0 +1,39 @@
+package com.shri.springify.Springify.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String jwt=request.getHeader("Authorization");
+
+        if(jwt!=null)
+        {
+            jwt=jwt.substring(7);
+            try{
+                SecretKey key= Keys.hmacShaKeyFor(JWT_CONSTANT.secret.getBytes());
+                Claims claims = Jwts.parser().setSigningKey(key).build().parseClaimsJws().getBody();
+
+            }
+            catch (Exception e)
+            {
+                throw new BadCredentialsException("Invalid JWT token");
+            }
+        }
+
+    }
+}
