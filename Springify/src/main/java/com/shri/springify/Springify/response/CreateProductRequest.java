526f289f0f7cf6d4d98a371bd9295093ac116901
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-02-12 20:08:14 +0530

    Product  service interface outline done

---
+++ Springify/src/main/java/com/shri/springify/Springify/response/CreateProductRequest.java
@@ -0,0 +1,25 @@
+package com.shri.springify.Springify.response;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateProductRequest {
+
+    private String title;
+    private  String description;
+    private int mrpPrice;
+    private int sellingPrice;
+
+    private  String color;
+    private List<String> images;
+
+    private  String category;
+
+    private String category2;
+
+    private String category3;
+
+    private  String size;
+}
---
+++ Springify/src/main/java/com/shri/springify/Springify/service/ProductService.java
@@ -0,0 +1,38 @@
+package com.shri.springify.Springify.service;
+
+import com.shri.springify.Springify.model.Product;
+import com.shri.springify.Springify.model.Seller;
+import com.shri.springify.Springify.response.CreateProductRequest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ProductService {
+    public Product createProduct(CreateProductRequest req, Seller seller);
+
+    public void deleteProduct(Long productId);
+
+    public Product updateProduct(Long productId,Product product);
+
+    Product findProductById(Long id);
+
+    List<Product> searchProducts();
+    public Page<Product> getAllProducts(
+
+            String category,
+            String brand,
+            String colors,
+            String size,
+            Integer maxPrice,
+            Integer minPrice,
+            Integer minDiscount,
+            String sort,
+
+            String stock,
+            Integer pageNumber
+    );
+
+
+    List<Product> getProductBySellerId(Long id);
+
+}
