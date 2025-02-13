b17397a1956a0949e13dfc98fd0e534231c26790
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2025-02-13 16:55:32 +0530

    order service outline done

---
+++ Springify/src/main/java/com/shri/springify/Springify/service/OrderService.java
@@ -0,0 +1,23 @@
+package com.shri.springify.Springify.service;
+
+import com.shri.springify.Springify.domain.OrderStatus;
+import com.shri.springify.Springify.model.*;
+
+import java.util.List;
+import java.util.Set;
+
+public interface OrderService {
+    Set<Order> createOrder(String jwt, Address shippingAddress, Cart cart);
+
+    Order findOrderById(Long id);
+
+    List<Order> usersOrderHistory(String jwt);
+
+    List<Order> sellersOrder(Long sellerId);
+
+    Order updateOrderStatus(Long orderId, OrderStatus orderStatus);
+
+    Order cancelOrder(Long orderId,String jwt);
+
+
+}
